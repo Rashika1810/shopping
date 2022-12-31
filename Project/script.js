3b1c34014fa693e9a9799eeceb254b9794d8e61a
Rashika <rashika.kr04@gmail.com>
2022-12-31 19:13:17 +0530

    final

--- Project/script.js
+++ Project/script.js
@@ -3,7 +3,7 @@ let signupBtn = document.getElementById("signupBtn");
 let nameField = document.getElementById("nameField");
 let title = document.getElementById("title");
 let signinBtn=document.getElementById("signinBtn");
-let loggedIn=document.getElementById("loggedIn");
+
 
 signinBtn.onclick= function()
 {
@@ -11,24 +11,21 @@ signinBtn.onclick= function()
     title.innerHTML="Sign In";
     signupBtn.classList.add("disable");
     signinBtn.classList.remove("disable");
+    
 }
 signupBtn.onclick= function()
 {
     nameField.style.maxHeight="60px";
     title.innerHTML="Sign Up";
+    
     signupBtn.classList.remove("disable");
     signinBtn.classList.add("disable");
 }
-loggedIn.onclick=function()
-{
-    alert("Logged In successfully!");
-}
 
-// function shopNow()
-// {
-//     document.getElementsByClassName("container")[0].style.display="visible";
 
-// }
+
+
+
 
 
 
